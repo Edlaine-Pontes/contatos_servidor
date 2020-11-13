a5c09f859b76adc3fe003635c6aaab51d35122ca
Edlaine Pontes <edlaine.psico@gmail.com>
2020-11-13 19:04:10 -0300

    atualização router

--- src/routes/contatosRoutes.js
+++ src/routes/contatosRoutes.js
@@ -6,4 +6,19 @@ const controller = require('../controller/contatosController')
 router.get('/', controller.getAll)
 router.post('/criar', controller.addContato)
 
+//retorna contato buscado por nome 
+router.get('/nome/:nome', controller.getByName)
+
+//retorna contato buscado por id
+router.get('/id/:id', controller.getById)
+
+//deleta contato
+router.delete('/deletar/:id', controller.deleteContact)
+
+//altera o campo 'celular' do cadastro
+router.put('/atualizar/telefone/:id', controller.updateNumberContact)
+
+//altera todo o cadastro
+router.put('/atualizar/:id', controller.updateContact)
+
 module.exports = router;
