101f5b62c5c6a971ad058606314a7dd6fc20e628
Edlaine Pontes <edlaine.psico@gmail.com>
2020-11-13 19:17:02 -0300

    app

--- src/routes/contatosRoutes.js
+++ src/routes/contatosRoutes.js
@@ -3,22 +3,46 @@ const router = express.Router()
 const controller = require('../controller/contatosController')
 
 
+// @route GET contatos
+// @desc Retornar todas os contatos
+// @access Public 
+// @endpoint http://localhost:porta/
 router.get('/', controller.getAll)
+
+// @route POST contato
+// @desc Criar um novo contato
+// @access Public 
+// @endpoint http://localhost:porta/criar
 router.post('/criar', controller.addContato)
 
-//retorna contato buscado por nome 
-router.get('/nome/:nome', controller.getByName)
+// @route GET nome
+// @desc Retorna contato por nome
+// @access Public 
+// @endpoint http://localhost:porta/nome
+router.get('/nome/:nome', controller.getByNome)
 
-//retorna contato buscado por id
+// @route GET id
+// @desc Retorna contato por id
+// @access Public 
+// @endpoint http://localhost:porta/id
 router.get('/id/:id', controller.getById)
 
-//deleta contato
-router.delete('/deletar/:id', controller.deleteContact)
+// @route Delete id
+// @desc deleta contato por id
+// @access Public 
+// @endpoint http://localhost:porta/deletar/
+router.delete('/deletar/:id', controller.deleteContato)
 
-//altera o campo 'celular' do cadastro
-router.put('/atualizar/telefone/:id', controller.updateNumberContact)
+// @route PUT telefone
+// @desc Alterar o campo celular do cadastro
+// @access Public 
+// @endpoint http://localhost:porta/atualizar/telefone/id
+router.put('/atualizar/telefone/:id', controller.updateNumContato)
 
-//altera todo o cadastro
-router.put('/atualizar/:id', controller.updateContact)
+// @route PUT Id
+// @desc alterar todo o cadastro
+// @access Public 
+// @endpoint http://localhost:porta/atualizar/id
+router.put('/atualizar/:id', controller.updateContato)
 
 module.exports = router;
